f5845b8c8fd9f4c9c722a0f988ec0d0bf8e59eec
Darío Gallegos <05dariogallegos@gmail.com>
2024-09-30 13:15:58 +0200

    Updated note model and tag model

--- NoteSwiftData/Models/NoteModel.swift
+++ NoteSwiftData/Models/NoteModel.swift
@@ -1,5 +1,5 @@
 //
-//  Note.swift
+//  NoteModel.swift
 //  NoteSwiftData
 //
 //  Created by Dario Gallegos on 18/9/24.
@@ -9,20 +9,37 @@ import Foundation
 import SwiftData
 
 @Model
-class Note {
-    @Attribute(.unique) var id: String?
+class NoteModel {
+    @Attribute(.unique) var id: String
     var content: String
     var createdAt: Date
     
     /// Cuando defines la relación como opcional ([Tags]?), SwiftData lo interpreta como una relación que puede o no existir.
     /// Sin embargo, cuando la defines como no opcional ([Tags]), estás indicando que siempre debe haber una relación válida con uno o más Tags.
     /// Si usas una relación no opcional, debes asegurarte de que la propiedad tags siempre tenga un valor, aunque sea  []
-    @Relationship(inverse: \Tags.notes) var tags: [Tags]?
+    @Relationship(inverse: \TagModel.notes) var tags: [TagModel]?
     
-    init(id: String, content: String, createdAt: Date, tags: [Tags]) {
+    init(id: String, content: String, createdAt: Date, tags: [TagModel]) {
         self.id = id
         self.content = content
         self.createdAt = createdAt
         self.tags = tags
     }
+}
+
+extension NoteModel {
+    var toNote: Note {
+        Note(
+            id: id,
+            content: content,
+            createAt: createdAt
+        )
+    }
+}
+
+
+struct Note: Identifiable {
+    let id: String
+    let content: String
+    let createAt: Date
 }
--- NoteSwiftData/Models/TagModel.swift
+++ NoteSwiftData/Models/TagModel.swift
@@ -1,5 +1,5 @@
 //
-//  Tag.swift
+//  TagModel.swift
 //  NoteSwiftData
 //
 //  Created by Dario Gallegos on 18/9/24.
@@ -9,19 +9,32 @@ import Foundation
 import SwiftData
 
 @Model
-class Tags {
-    @Attribute(.unique) var id: String?
+class TagModel {
+    @Attribute(.unique) var id: String
     var name: String
     
 //        .nullify: Al eliminar un Tag, las referencias en las Notes se pondrán a nil, pero las Notes no se eliminarán.
 //        .cascade: Al eliminar un Tag, las Notes relacionadas también se eliminarán. (No es lo que quieres en este caso).
 //        .deny: Impide la eliminación de un Tag si está asociado a alguna Note.
 //        .Manual: Es la opción que estamos aplicando, donde eliminas manualmente el Tag de los arrays tags de las Notes antes de borrarlo.
-    @Relationship(deleteRule: .nullify) var notes: [Note]?
+    @Relationship(deleteRule: .nullify) var notes: [NoteModel]?
     @Attribute(.ephemeral) var isChecked = false
     
     init(id: String, name: String) {
         self.id = id
         self.name = name
+    }
+}
+
+
+struct Tag: Identifiable {
+    let id: String
+    let name: String
+    let notes: [Note]?
+    
+    init(id: String, name: String, notes: [Note]? = []) {
+        self.id = id
+        self.name = name
+        self.notes = notes
     }
 }
